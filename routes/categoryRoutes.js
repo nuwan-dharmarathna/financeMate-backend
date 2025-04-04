4d8c6494def71a87a1021c85f642dd8d6ea38540
Uchith Chethana <wgucgjayarathna9898@gmail.com>
2025-04-04 08:32:49 +0530

    category function

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const categoryController = require('../controllers/categoryController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+router
+  .route('/')
+  .get(categoryController.getAllCategories)
+  .post(categoryController.createCategory);
+
+router
+  .route('/:id')
+  .get(categoryController.getCategory)
+  .patch(categoryController.updateCategory)
+  .delete(categoryController.deleteCategory);
+
+module.exports = router;
